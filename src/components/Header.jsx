97598548bc1a62d6b04f986897de01636c32e094
rejennylo <rejennylo@gmail.com>
2023-10-26 22:30:20 +0800

    style: adjust header CSS styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ export const Header = () => {
   const navItems = ["正式連載", "分類", "人氣排行榜", "投稿新星專區"];
   return (
     <header>
-      <div className="flex mx-auto items-center max-w-screen-xl relative">
+      <div className="flex mx-auto items-center max-w-screen-xl relative pl-5">
         <h1>
           <a href="#"></a>
         </h1>
-        <ul className="flex items-center my-8 absolute right-0">
+        <ul className="flex items-center my-8 absolute right-0 mr-5">
           <li className="flex justify-center items-center border-solid border-2 rounded-full ml-3 h-9 w-28 bg-gray-800 hover:bg-black">
             <a href="#" className="text-sm text-white">
               上傳作品
@@ -28,7 +28,7 @@ export const Header = () => {
             />
           </li>
         </ul>
-        <div className="hidden flex items-center justify-between my-8 absolute right-0 border-solid border border-gray-300 rounded-full ml-3 h-9 w-[330px] px-3 bg-white">
+        <div className="hidden flex items-center justify-between my-8 absolute right-0 border-solid border border-gray-300 rounded-full ml-3 h-9 w-[330px] px-3 mr-5 bg-white">
           <span className="inline-block">
             <input
               type="text"
